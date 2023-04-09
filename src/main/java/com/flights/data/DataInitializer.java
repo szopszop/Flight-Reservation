44eb3fbecde317622cde03228949d3e9b9c63066
szopszop <szymontracz1@gmail.com>
2023-04-09 22:08:49 +0200

    reafactoring - moved transferAirportsToDatabase to AirportService

--- src/main/java/com/flights/data/DataInitializer.java
+++ src/main/java/com/flights/data/DataInitializer.java
@@ -1,26 +1,12 @@
 package com.flights.data;
 
-import com.flights.entities.Airport;
-import com.flights.mapper.AirportMapper;
-import com.flights.repositories.AirportRepository;
 import com.flights.service.AirportService;
-import com.opencsv.exceptions.CsvException;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 @Component
 public class DataInitializer {
-
-    @Autowired
-    private AirportRepository airportRepository;
-
-    @Autowired
-    private AirportMapper airportMapper;
 
     @Autowired
     private AirportService airportService;
@@ -28,30 +14,9 @@ public class DataInitializer {
 
     @PostConstruct
     public void init() {
-
-        transferAirportsToDatabase();
+        airportService.transferAirportsToDatabase();
 
     }
 
-    private void transferAirportsToDatabase() {
-        List<String[]> filteredAirports = airportService.getFilteredAirports();
 
-
-        List<Airport> airports = new ArrayList<>();
-        int lineNumber = 0;
-        for (String[] airportData : filteredAirports) {
-            try {
-                Airport airport = airportMapper.mapToAirport(airportData);
-                airports.add(airport);
-            } catch (NumberFormatException e) {
-                System.err.printf("Błąd przetwarzania danych w linii %d: %s%n", lineNumber, e.getMessage());
-                for (String s :airportData) {
-                    System.out.println(s);
-                }
-                e.printStackTrace();
-            }
-            lineNumber++;
-        }
-        airportRepository.saveAll(airports);
-    }
 }
--- src/main/java/com/flights/service/AirportService.java
+++ src/main/java/com/flights/service/AirportService.java
@@ -2,9 +2,13 @@ package com.flights.service;
 
 import com.flights.csv.CsvReader;
 import com.flights.csv.DataFilter;
+import com.flights.entities.Airport;
+import com.flights.mapper.AirportMapper;
+import com.flights.repositories.AirportRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -14,6 +18,8 @@ public class AirportService {
 
     private final CsvReader csvReader;
     private final DataFilter dataFilter;
+    private final AirportMapper airportMapper;
+    private final AirportRepository airportRepository;
     private static final String HUM_DATA_FILE_PATH = "src/main/resources/humData/airports-humData.csv";
     private static final String OPEN_FLIGHTS_FILE_PATH = "src/main/resources/openFlights/airports-openFlights.csv";
     private static final int LARGE_AIRPORT_COLUMN = 2;
@@ -44,7 +50,25 @@ public class AirportService {
         Set<String> IATACodes = getIATACodes();
         return dataFilter.doFilter(allAirportsFromOpenData, IATA_CODE_COLUMN, IATACodes);
     }
+    public void transferAirportsToDatabase() {
+        List<String[]> filteredAirports = filterAirportByIATA();
 
+
+        List<Airport> airports = new ArrayList<>();
+        int lineNumber = 0;
+        for (String[] airportData : filteredAirports) {
+            try {
+                Airport airport = airportMapper.mapToAirport(airportData);
+                airports.add(airport);
+            } catch (NumberFormatException e) {
+                System.err.printf("Error during processing at line %d: %s%n", lineNumber, e.getMessage());
+
+                e.printStackTrace();
+            }
+            lineNumber++;
+        }
+        airportRepository.saveAll(airports);
+    }
 
 
 
