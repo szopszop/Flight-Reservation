08019bba2a1a7ccb8e0dca85b58aeeb9ff5bd9a2
szopszop <szymontracz1@gmail.com>
2023-04-09 21:57:47 +0200

    AirportService refactor

---
+++ src/main/java/com/flights/entities/Airline.java
@@ -0,0 +1,32 @@
+package com.flights.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "airlines")
+public class Airline {
+    @Id
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "alias")
+    private String alias;
+    @Column(name = "iata_code")
+    private String IATA_Code;
+    @Column(name = "icao_code")
+    private String ICAO_Code;
+    @Column(name = "callsign")
+    private String callSign;
+    @Column(name = "country")
+    private String country;
+}
--- src/main/java/com/flights/service/AirportService.java
+++ src/main/java/com/flights/service/AirportService.java
@@ -14,32 +14,35 @@ public class AirportService {
 
     private final CsvReader csvReader;
     private final DataFilter dataFilter;
+    private static final String HUM_DATA_FILE_PATH = "src/main/resources/humData/airports-humData.csv";
+    private static final String OPEN_FLIGHTS_FILE_PATH = "src/main/resources/openFlights/airports-openFlights.csv";
+    private static final int LARGE_AIRPORT_COLUMN = 2;
+    private static final int IATA_CODE_COLUMN = 4;
+    private static final int KEY_COLUMN = 13;
 
+    private List<String[]> importAirportDataFromHumData() {
+        return csvReader.readFile(HUM_DATA_FILE_PATH);
+    }
 
-    public List<String[]> getLargeAirports() {
-        String airportsCsvFilePath = "src/main/resources/humData/airports-humData.csv";
-        List<String[]> unfilteredAirportsData = csvReader.readFile(airportsCsvFilePath);
-        int row = 2;
+    public List<String[]> filterLargeAirports() {
+        List<String[]> unfilteredAirportsData = importAirportDataFromHumData();
         String filter = "large_airport";
-        return dataFilter.doFilter(unfilteredAirportsData, row, filter);
+        return dataFilter.doFilter(unfilteredAirportsData, LARGE_AIRPORT_COLUMN, filter);
     }
 
     public Set<String> getIATACodes() {
-        List<String[]> largeAirports = getLargeAirports();
-        int column = 13;
-        return dataFilter.getKeys(largeAirports, column);
+        List<String[]> largeAirports = filterLargeAirports();
+        return dataFilter.getKeys(largeAirports, KEY_COLUMN);
     }
 
-    public List<String[]> getAllAirportsOpenFlight() {
-        String airportsCsvFilePath = "src/main/resources/openFlights/airports-openFlights.csv";
-        return csvReader.readFile(airportsCsvFilePath);
+    public List<String[]> importAirportDataFromOpenFlights() {
+        return csvReader.readFile(OPEN_FLIGHTS_FILE_PATH);
     }
 
-    public List<String[]> getFilteredAirports() {
-        List<String[]> allAirportsFromOpenData = getAllAirportsOpenFlight();
+    public List<String[]> filterAirportByIATA() {
+        List<String[]> allAirportsFromOpenData = importAirportDataFromOpenFlights();
         Set<String> IATACodes = getIATACodes();
-        int column = 4;
-        return dataFilter.doFilter(allAirportsFromOpenData, column, IATACodes);
+        return dataFilter.doFilter(allAirportsFromOpenData, IATA_CODE_COLUMN, IATACodes);
     }
 
 
